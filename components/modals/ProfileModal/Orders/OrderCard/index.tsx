c786f6fb86378a8020a8173d10e308b419028103
NOTAPC\NotAUser2000 <sewardwoden@gmail.com>
2023-02-21 18:03:01 +0600

    OrderCard

--- components/modals/ProfileModal/Orders/OrderCard/index.tsx
+++ components/modals/ProfileModal/Orders/OrderCard/index.tsx
@@ -1,15 +1,38 @@
+import { IOrder } from 'data/interfaces/IOrder'
+import Image from 'next/image'
 import styles from './index.module.scss'
 
 
 interface Props {
-
+  order: IOrder
 }
 
-export default function OrderCard(props: Props) {
+export default function OrderCard({ order }: Props) {
 
   return (
     <div className={styles.root}>
-      
+      <div className={styles.right}>
+        <div className={styles.brand}>
+          {order.brand.name}
+        </div>
+        <div className={styles.discount}>
+          <div><Image src={'/images/icons/discount.svg'} alt='' fill /></div>
+          <div className={styles.percent}>Скидка {order.totalDiscount}%</div>
+        </div>
+        <div className={styles.created}>
+          {order.createdAt}
+        </div>
+      </div>
+      <div className={styles.left}>
+        <div className={styles.payment}>
+          <div className={styles.method}>
+            {order.paymentMethod}
+          </div>
+          <div className={styles.total}>
+            {order.total}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- components/modals/ProfileModal/Orders/index.tsx
+++ components/modals/ProfileModal/Orders/index.tsx
@@ -1,4 +1,8 @@
+import { DeliveryMethod } from 'data/enum/DeliveryMethod'
+import { PaymentMethod } from 'data/enum/PaymentMethod'
+import { PriceRating } from 'data/enum/PriceRating'
 import styles from './index.module.scss'
+import OrderCard from './OrderCard'
 
 
 interface Props {
@@ -7,13 +11,70 @@ interface Props {
 
 export default function Orders(props: Props) {
 
+  const orders = [
+    {
+      id: '1',
+      number: '1',
+      partnerNumber: '1',
+      address: {
+        id: '1',
+        address: '',
+        city: 'Moscow',
+        street: 'Pushkina st.',
+        house: '1',
+      },
+      location: {
+        lat: 1,
+        lng: 2,
+      },
+      paymentMethod: PaymentMethod.Cash,
+      deliveryMethod: DeliveryMethod.Delivery,
+      email: '',
+      phone: '',
+      personsCount: 666,
+      createdAt: '10.10.2010',
+      deliveredAt: '',
+      subTotal: 1,
+      totalDiscount: 20,
+      total: 100000,
+      isPreOrder: false,
+      preOrderAt: new Date(),
+      brand: {
+        id: 1,
+        name: 'Burger King',
+        slug: 'burger_king',
+        priceRating: PriceRating.Cheap
+      },
+      moneyChange: 0,
+      isContactLessDelivery: false,
+      lines: [{
+        id: '1',
+        name: 'pizza',
+        modificationLines: [{
+          id: '1',
+          name: 'name',
+          modificationId: '1',
+          quantity: 1
+        }],
+        total: 1,
+        price: 100,
+        quantity: 100,
+        productId: 1,
+        isWeighted: true,
+        modificationsPrice: 1
+      }],
+    }
+  ]
+
   return (
     <div className={styles.root}>
       <div className={styles.title}>
         История заказов
       </div>
       <div className={styles.container}>
-        
+        {orders.map((i, index) =>
+          <OrderCard order={i} key={index} />
+        )}
       </div>
     </div>
   )
