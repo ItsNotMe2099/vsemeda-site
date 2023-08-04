97ea4109dec85edac6f5d054df6854053b8022b8
tetpic <rabustes@gmail.com>
2023-08-04 10:53:54 +0300

    scroll away feature on mobile on mainpage

--- components/for_pages/index/BestOffers/index.tsx
+++ components/for_pages/index/BestOffers/index.tsx
@@ -1,31 +1,46 @@
-import styles from 'components/for_pages/index/BestOffers/index.module.scss'
+import styles from './index.module.scss'
 import DropdownMenu from 'components/ui/DropdownMenu'
 import { ICategory } from 'data/interfaces/ICategory'
 import MenuRepository from 'data/repositories/MenuRepository'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import UnitSlider from 'components/for_pages/Common/UnitSlider'
 import {IViewLayoutItem} from 'data/interfaces/IViewLayout'
 import VisibleOnSize from 'components/visibility/VisibleOnSize'
 import { breakpoints } from 'styles/variables'
+import { useThrottleFn } from '@react-cmpt/use-throttle'
+import { useResize } from 'components/hooks/useResize'
 interface Props{
  item: IViewLayoutItem
 }
 export default function BestOffers(props: Props) {
 
   const [items, setItems] = useState<ICategory[] | null>(null)
+  const offersRef = useRef<HTMLDivElement>(null!)
+  const {isPhoneWidth} = useResize()
+
 
   const fetchData = async () => {
     await MenuRepository.fetchCategories().then(i => setItems(i))
   }
+  
+  const throt = useThrottleFn((e, top)=> {
+    const topPos = offersRef.current.getBoundingClientRect().top
+    offersRef.current.style.cssText = `transform: translateX(${(top - topPos)*1.5}px);`
+    
+  }, 50)
 
-
+  const scrollProgress = () => {
+    const top = offersRef.current.getBoundingClientRect().top
+    window.addEventListener('scroll', (e) => {throt.callback(e, top)})
+  }
 
   useEffect(() => {
     fetchData()
+    isPhoneWidth&&scrollProgress()
   }, [])
 
   return (
-      <div className={styles.root}>
+      <div className={styles.root} ref={offersRef}>
         <div className={styles.left}>
           <div className={styles.title}>
             Лучшие<br /> рестораны
