a4f2290c16cc1242d223830261703a9daefbd9fe
tetpic <rabustes@gmail.com>
2023-09-29 08:14:24 +0300

    fast name fix

--- components/layout/Header/UserMenu/index.tsx
+++ components/layout/Header/UserMenu/index.tsx
@@ -73,7 +73,7 @@ export default function UserMenu(props: Props) {
       dropdownRef.current = ref
       setReferenceElement(ref)
     }}>
-      <div className={styles.name}>{appContext.user ? appContext.user.name.split(' ')[0] : ''}</div>  
+      <div className={styles.name}>{appContext.user?.name ? appContext.user?.name?.split(' ')[0] : appContext.user?.name}</div>  
       {/* <div className={styles.name}>{appContext.user ? appContext.user.name: ''}</div>   */}
       <ChevronSvg className={styles.chevron} color={colors.white} />
       <div className={styles.icon}>
